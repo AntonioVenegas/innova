2e07ded7503022b189981f05b7969ba9d4b142b7
Antonio Venegas <antonio.venegas@uc.cl>
2024-05-06 12:07:10 -0400

    progreso

--- clases.py
+++ clases.py
@@ -1,27 +1,52 @@
 class Producto():
     id = 0
     def __init__(self):
+        id += 1
+        self.id = id
 
 class Etapa():
     id = 0
     def __init__(self):
         self.productos = []
+        id += 1
+        self.id = id
 
-class Operacion():
+class Operacion(self, factor_emision=float):
     id = 0
     def __init__(self):
-        self.productos = []
+        self.etapas = []
+        self.factor_emision = factor_emision
+        id += 1
+        self.id = id
+    
+    def add_etapa(self):
+        pass
 
 class Transporte():
     id = 0
-    def __init__(self):
-        pass
+    def __init__(self, insumo=Producto, factor_emision=float):
+        self.insumo = insumo
+        self.factor_emision = factor_emision
+        id += 1
+        self.id = id
 
 class Proveedor():
     id = 0
     def __init__(self):
-        self.transporte = []
+        self.transportes = []
         self.operaciones = []
+        id += 1
+        self.id = id
+
+    def get_total_emitions(self):
+        total = 0
+        for t in self.transportes:
+            total += t.factor_emision
+
+        for o in self.operaciones:
+            total += t.factor_emision
+
+
 
 class Empresa():
     id = 0
--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # acá ira nuestro proyecto pro
 #hola?
+import clases
+from pprint import pprint as pp
+
+if __name__ == '__main__':
+    control = True
+    while control:
+        command = input("Seleccione opción: \n")
+        if command == "seguir":
+            pass
+        else:
+            control = False
